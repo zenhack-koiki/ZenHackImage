a2d1ff6c86833a519936df8ebae6a139a159368f
nabuchi <nabnab.main+github@gmail.com>
2016-10-02 07:12:34 +0900

    change url

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
   
   get 'recommend/index'
 
-  resources :images
   resources :likes do
     collection do
       get 'search'
     end
   end
 
-  get '/apis/zenhack/images', to: 'images#search'
+  get '/images', to: 'images#search'
 
 end
