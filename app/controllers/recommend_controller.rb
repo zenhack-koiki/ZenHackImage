fd89dc975881823fbeabb3faf352cf5df1eb83f6
nabuchi <nabnab.main+github@gmail.com>
2016-10-02 10:53:51 +0900

    add spot

--- app/controllers/recommend_controller.rb
+++ app/controllers/recommend_controller.rb
@@ -4,12 +4,14 @@ class RecommendController < ApplicationController
     @likes = Like.where(:session_id => session_id, :is_like => true)
     @recommends = []
     @likes.each do |like|
-      url = "https://graph.facebook.com/v2.7/search?access_token=EAACEdEose0cBANkE1vl0oG8kDFN4oJjIZBiy9gr2wuOwEEGlnscZCuMZACOjnN2RGfuk9uCYnpCm2cv6UO8yn9qHvUfNU6f4mBMcJjMGZAJYY2vbdn0heYNI4M2vDR4OHyxjrnqARjksgpWqRA6VJyOGl8LkXXiJZCRejgE64KAZDZD&center=#{like.image.latitude}%2C#{like.image.longitude}&debug=all&distance=1000&format=json&method=get&pretty=0&suppress_http_code=1&type=place"
+      url = "https://graph.facebook.com/v2.7/search?access_token=EAACEdEose0cBAA3nbwlTVGrWcS8OZAAxGDNLymzCCtJOamZCS6ZBhCUfvNBmxnMDRltVlpKHHzNpqoEx3zrhCJMzsonHSDsPCpuTwZAjcccd6fZBn9H67ZBGTZAl3Gc5b74h8QfywB7CgK4ckTptZBtdE7idpGdWwqa5nNJYcq0NawZDZD&center=#{like.image.latitude}%2C#{like.image.longitude}&debug=all&distance=1000&format=json&method=get&pretty=0&suppress_http_code=1&type=place"
       spot = JSON.parse(RestClient.get(url))
       #@recommends << {:longitude => like.image.longitude, :latitude => like.image.latitude, :spot => spot['data'][0]['name']}
       logger.info like.image
+      logger.info url
       logger.info spot
-      @recommends << {:longitude => like.image.longitude, :latitude => like.image.latitude, :url => like.image.url}
+      spot_name = spot['data'][0]['name'] unless spot['data'].nil?
+      @recommends << {:longitude => like.image.longitude, :latitude => like.image.latitude, :url => like.image.url, :spot => spot_name}
     end
   end
 end
