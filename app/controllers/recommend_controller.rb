a1752e7ded78b468c3fa4022472584056a22d443
nabuchi <nabnab.main+github@gmail.com>
2016-10-02 09:11:00 +0900

    bugfix

--- app/controllers/recommend_controller.rb
+++ app/controllers/recommend_controller.rb
@@ -7,8 +7,8 @@ class RecommendController < ApplicationController
       url = "https://graph.facebook.com/v2.7/search?access_token=EAACEdEose0cBANkE1vl0oG8kDFN4oJjIZBiy9gr2wuOwEEGlnscZCuMZACOjnN2RGfuk9uCYnpCm2cv6UO8yn9qHvUfNU6f4mBMcJjMGZAJYY2vbdn0heYNI4M2vDR4OHyxjrnqARjksgpWqRA6VJyOGl8LkXXiJZCRejgE64KAZDZD&center=#{like.image.latitude}%2C#{like.image.longitude}&debug=all&distance=1000&format=json&method=get&pretty=0&suppress_http_code=1&type=place"
       spot = JSON.parse(RestClient.get(url))
       #@recommends << {:longitude => like.image.longitude, :latitude => like.image.latitude, :spot => spot['data'][0]['name']}
-      logger like.image
-      logger spot
+      logger.info like.image
+      logger.info spot
       @recommends << {:longitude => like.image.longitude, :latitude => like.image.latitude, :url => like.image.url}
     end
   end
